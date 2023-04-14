31af16428e080ce26c9fda454420f2efe620cc37
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-14 17:19:33 +0800

    fix: component location

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Navbar from '@/components/Navbar/Navbar';
 
 export default function Document() {
   return (
@@ -8,6 +9,7 @@ export default function Document() {
     >
       <Head />
       <body>
+        <Navbar />
         <Main />
         <NextScript />
       </body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import Navbar from '@/components/Navbar/Navbar';
+import Hero from '@/components/Hero/Hero';
 
 export default function Home() {
   return (
     <main>
-      <Navbar />
+      <Hero />
     </main>
   );
 }
