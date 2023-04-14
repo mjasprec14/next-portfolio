73b90ece34fe60694ce06c882ad45ea91af004c0
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-14 18:29:39 +0800

    feat: Head in index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import Hero from '@/components/Hero/Hero';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <main>
+      <Head>
+        <title>MJ Asprec&apos;s Portfolio</title>
+      </Head>
       <Hero />
     </main>
   );
