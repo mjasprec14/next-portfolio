adbfd08dd1e54bb7ecfaec5a7a2557f334322524
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-15 02:14:51 +0800

    feat: tailwind config for slideup animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,21 @@ module.exports = {
           'bounce 0.5s alternate cubic-bezier(0.95, 0.05, 0.795, 0.035) infinite',
         slideUpCubiBezier: 'slideUp 1s cubic-bezier(0.165, 0.84, 0.44, 1)',
       },
+      animationDelay: {
+        0: '0s',
+        2: '0.2s',
+        4: '0.4s',
+        6: '0.6s',
+      },
       keyframes: {
+        fadeIn: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+        slideUp: {
+          from: { transform: 'translateY(100%)' },
+          to: { transform: 'translateY(0)' },
+        },
         bounce: {
           from: { transform: 'translateY(10px)' },
           to: { transform: 'translateY(0)' },
