4ee4d54f7dc6a99068cb6a916e04082e9fa2041e
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-14 23:44:19 +0800

    feat: SlideUp component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,3 +2,4 @@ export { default as About } from './About/About';
 export { default as Hero } from './Hero/Hero';
 export { default as Navbar } from './Navbar/Navbar';
 export { default as Projects } from './Projects/Projects';
+export { default as SlideUp } from './SlideUp/SlideUp';
